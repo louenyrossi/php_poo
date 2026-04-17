9df9673a77b3d272d52c0f7a3386b00f4e1b38a5
Loueny <loueny.rossi@aluno.senai.br>
2026-04-17 08:34:09 -0300

    Carro atualizado

--- carro.php
+++ carro.php
@@ -26,8 +26,8 @@ class Carro {
 $meuCarro = new Carro("Senai-Mobile", 0);
 
 // O desastre: alteração direta sem validação
-$meuCarro->getVelocidade(5000); // Velocidade de foguete?
-$meuCarro->setVelocidade(-60);   // Carro andando no tempo?
+$meuCarro->setVelocidade(5000); // Velocidade de foguete?
+$meuCarro->getVelocidade(-60);   // Carro andando no tempo?
 
 echo "Modelo: " . $meuCarro->modelo . "<br>";
 echo "Velocidade atual: " . $meuCarro->getVelocidade() . " km/h";
